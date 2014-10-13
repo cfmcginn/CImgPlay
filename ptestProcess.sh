92d6876d86bb449e9bb2d3e1985b85ab3543aacf
Christopher McGinn <cfmcginn@Christophers-Mac-mini.local>
2014-10-13 17:59:03 +0200

    Update cpp

--- ptestProcess.sh
+++ ptestProcess.sh
@@ -8,8 +8,8 @@ fi
 now="imageJob_$(date +"%m_%d_%Y__%H_%M_%S")"
 mkdir $now
 
-NAME="testProcess.C"
-g++ $NAME -Werror -Wall -O2 -o "${NAME/%.C/}.exe" -I/usr/X11R6/include -L/usr/X11R6/lib -lX11
+NAME="testProcess.cpp"
+g++ $NAME -Werror -Wall -O2 -o "${NAME/%.cpp/}.exe" -I/usr/X11R6/include -L/usr/X11R6/lib -lX11
 
 mv *.exe $now
 cp testPhoto.jpg $now
--- testProcess.cpp
+++ testProcess.cpp
@@ -4,7 +4,9 @@ int main()
 {
   cimg_library::CImg<unsigned char> image("testPhoto.jpg");
   //  const unsigned char red[] = {255,0,0}, green[] = {0,255,0}, blue[] = {0,0,255};
-  image.blur(2.5);
+  image.blur(10);
+
+  image.display("Blurumbo");
 
   return 0;
 }
