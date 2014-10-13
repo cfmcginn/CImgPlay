ce57213ad5ae364e6097efdd381c978e0cfa72c2
Christopher McGinn <cfmcginn@Christophers-Mac-mini.local>
2014-10-13 19:27:09 +0200

    basic grayscale

--- testProcess.cpp
+++ testProcess.cpp
@@ -3,6 +3,10 @@
 
 int main()
 {
+  const float redWeight = 0.2989;
+  const float greenWeight = 0.5870;
+  const float blueWeight = 0.1140;
+
   cimg_library::CImg<unsigned char> img("testPhoto.jpg");
 
   const unsigned int imgWidth = img.width();
@@ -17,9 +21,23 @@ int main()
 
   cimg_library::CImg<unsigned char> imgMod(imgWidth, imgHeight, imgDepth, imgSpec, 255);
 
+  for(int widthIter = 0; widthIter < imgWidth; widthIter++){
+    for(int heightIter = 0; heightIter < imgHeight; heightIter++){
+
+      float lum = (float)img(widthIter, heightIter, 0, 0)*redWeight + (float)img(widthIter, heightIter, 0, 1)*greenWeight + (float)img(widthIter, heightIter, 0, 2)*blueWeight;
+
+      imgMod(widthIter, heightIter, 0, 0) = (unsigned char)lum;
+      imgMod(widthIter, heightIter, 0, 1) = (unsigned char)lum;
+      imgMod(widthIter, heightIter, 0, 2) = (unsigned char)lum;
+
+    }
+  }
+
   unsigned char purple[3] = {255, 0, 255};
   img.draw_text(100, 100, "YOLO", purple);
   imgMod.draw_text(100, 100, "YOLO", purple);
+
+  imgMod.blur(2.5);
 
   img.save("testPhoto_YOLO.jpg");
   imgMod.save("testPhoto_YOLOMOD.jpg");
