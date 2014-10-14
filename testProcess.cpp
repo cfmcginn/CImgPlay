e79103b52cf3e3e6cba962af7228eaa971b6f866
Chris <ChrisMcGinn@Macintosh.local>
2014-10-13 21:50:06 -0400

    ADded randoms

--- testProcess.cpp
+++ testProcess.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <set>
+#include <stdlib.h>
 #include "CImg/CImg.h"
+//#include "boost_1_56_0/multi_array.hpp"
 
 void rgbToGrey(cimg_library::CImg<unsigned char>* inRGB_p, cimg_library::CImg<unsigned char>& outGrey);
 
@@ -23,15 +26,29 @@ int main(int argc, char* argv[])
   std::cout << "Image spectrum: " << imgSpec << std::endl;
 
   cimg_library::CImg<unsigned char> imgMod(imgWidth, imgHeight, imgDepth, imgSpec, 255);
-
   rgbToGrey(&img, imgMod);
 
   unsigned char purple[3] = {255, 0, 255};
   img.draw_text(100, 100, "YOLO", purple);
   imgMod.draw_text(100, 100, "YOLO", purple);
 
+  const int pixNum = 1000;
+  std::set<int>* pixSet_p = new std::set<int>;
+
+  while(pixSet_p->size() < pixNum){
+    int inNum = rand()%imgWidth*1000 + rand()%imgHeight;
+    pixSet_p->insert(inNum);
+  }
+
+  for(std::set<int>::iterator it = pixSet_p->begin(); it != pixSet_p->end(); it++){
+    std::cout << *it << std::endl;
+  }
+
   img.save("testPhoto_YOLO.jpg");
   imgMod.save("testPhoto_YOLOMOD.jpg");
+
+  pixSet_p->clear();
+  delete pixSet_p;
 
   return 0;
 }
@@ -39,20 +56,23 @@ int main(int argc, char* argv[])
 
 void rgbToGrey(cimg_library::CImg<unsigned char>* inRGB_p, cimg_library::CImg<unsigned char>& outGrey)
 {
-  const float redWeight = 0.2989;
-  const float greenWeight = 0.5870;
-  const float blueWeight = 0.1140;
+  const int rgbNum = 3;
+  const float rgbWeight[rgbNum] = {0.2989, 0.5870, 0.1140};
 
   const unsigned int imgWidth = inRGB_p->width();
   const unsigned int imgHeight = inRGB_p->height();
 
   for(int widthIter = 0; widthIter < imgWidth; widthIter++){
     for(int heightIter = 0; heightIter < imgHeight; heightIter++){
-      float lum = (float)inRGB_p->atXYZ(widthIter, heightIter, 0, 0)*redWeight + (float)inRGB_p->atXYZ(widthIter, heightIter, 0, 1)*greenWeight + (float)inRGB_p->atXYZ(widthIter, heightIter, 0, 2)*blueWeight;
+      float lum = 0.0;
 
-      outGrey(widthIter, heightIter, 0, 0) = (unsigned char)lum;
-      outGrey(widthIter, heightIter, 0, 1) = (unsigned char)lum;
-      outGrey(widthIter, heightIter, 0, 2) = (unsigned char)lum;
+      for(int lumIter = 0; lumIter < rgbNum; lumIter++){
+	lum += (float)inRGB_p->atXYZ(widthIter, heightIter, 0, lumIter)*rgbWeight[lumIter];
+      }
+
+      for(int lumIter = 0; lumIter < rgbNum; lumIter++){
+	outGrey(widthIter, heightIter, 0, lumIter) = (unsigned char)lum;
+      }
     }
   }
 
