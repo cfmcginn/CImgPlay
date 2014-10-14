cb06f9cacdac8b315f033dfa62bb8dc123d7c8f1
Christopher McGinn <cfmcginn@Christophers-Mac-mini.local>
2014-10-14 22:33:41 +0200

    update

--- ptestProcess.sh
+++ ptestProcess.sh
@@ -10,7 +10,7 @@ now="imageJob_$(date +"%m_%d_%Y__%H_%M_%S")"
 mkdir $now
 
 NAME="testProcess.cpp"
-g++ $NAME -Werror -Wall -O2 -o "${NAME/%.cpp/}.exe" -I/usr/X11R6/include -L/usr/X11R6/lib -lX11 -I/Users/ChrisMcGinn/CPPLibraries
+g++ $NAME -Werror -Wall -O2 -o "${NAME/%.cpp/}.exe" -I/usr/X11R6/include -L/usr/X11R6/lib -lX11 -I/Users/cfmcginn/CPPLibraries
 
 mv *.exe $now
 cp $1 $now
--- testProcess.cpp
+++ testProcess.cpp
@@ -1,52 +1,54 @@
 #include <iostream>
 #include <set>
+#include <vector>
+#include <algorithm>
 #include <stdlib.h>
 #include "CImg/CImg.h"
-//#include "boost_1_56_0/multi_array.hpp"
 
 void rgbToGrey(cimg_library::CImg<unsigned char>* inRGB_p, cimg_library::CImg<unsigned char>& outGrey);
+float getMedianGreyScale(cimg_library::CImg<unsigned char>* inGrey_p);
 
 int main(int argc, char* argv[])
 {
-  if(argc!=2){
-    std::cout << "Usage: testProcess <inFileName>" << std::endl;
+  if(argc!=3){
+    std::cout << "Usage: testProcess <inFileName> <nPhoto>" << std::endl;
     return 1;
   }
 
   cimg_library::CImg<unsigned char> img(argv[1]);
-
   const unsigned int imgWidth = img.width();
   const unsigned int imgHeight = img.height();
   const unsigned int imgDepth = img.depth();
   const unsigned int imgSpec = img.spectrum();
-
-  std::cout << "Image width: " << imgWidth << std::endl;
-  std::cout << "Image height: " << imgHeight << std::endl;
-  std::cout << "Image depth: " << imgDepth << std::endl;
-  std::cout << "Image spectrum: " << imgSpec << std::endl;
-
   cimg_library::CImg<unsigned char> imgMod(imgWidth, imgHeight, imgDepth, imgSpec, 255);
   rgbToGrey(&img, imgMod);
 
-  unsigned char purple[3] = {255, 0, 255};
-  img.draw_text(100, 100, "YOLO", purple);
-  imgMod.draw_text(100, 100, "YOLO", purple);
+  float medianLum = 0;
+  medianLum = getMedianGreyScale(&imgMod);
 
-  const int pixNum = 1000;
+  const int nOutPic = std::atoi(argv[2]);
+  int pixNum[nOutPic];
+  cimg_library::CImg<unsigned char>* imgSamples[nOutPic];
+  for(int pixIter = 0; pixIter < nOutPic; pixIter++){
+    pixNum[pixIter] = pixIter*imgWidth*imgDepth/nOutPic;
+
+    imgSamples[pixIter] = new cimg_library::CImg<unsigned char>
+  }
+
+
   std::set<int>* pixSet_p = new std::set<int>;
-
-  while(pixSet_p->size() < pixNum){
+  while(pixSet_p->size() < imgWidth*imgDepth*(nOutPic-1)/nOutPic){
     int inNum = rand()%imgWidth*1000 + rand()%imgHeight;
     pixSet_p->insert(inNum);
   }
 
+  /*
   for(std::set<int>::iterator it = pixSet_p->begin(); it != pixSet_p->end(); it++){
-    std::cout << *it << ", " << (*it)/1000 << ", " << (*it)%1000<< std::endl;
-
     for(int rgbIter = 0; rgbIter < 3; rgbIter++){
       imgMod((*it)/1000, (*it)%1000, 0, rgbIter) = 255;
     }
   }
+  */
 
   img.save("testPhoto_YOLO.jpg");
   imgMod.save("testPhoto_YOLOMOD.jpg");
@@ -66,19 +68,39 @@ void rgbToGrey(cimg_library::CImg<unsigned char>* inRGB_p, cimg_library::CImg<un
   const unsigned int imgWidth = inRGB_p->width();
   const unsigned int imgHeight = inRGB_p->height();
 
-  for(int widthIter = 0; widthIter < imgWidth; widthIter++){
-    for(int heightIter = 0; heightIter < imgHeight; heightIter++){
+  for(int wIter = 0; wIter < imgWidth; wIter++){
+    for(int hIter = 0; hIter < imgHeight; hIter++){
       float lum = 0.0;
 
       for(int lumIter = 0; lumIter < rgbNum; lumIter++){
-	lum += (float)inRGB_p->atXYZ(widthIter, heightIter, 0, lumIter)*rgbWeight[lumIter];
+	lum += (float)inRGB_p->atXYZ(wIter, hIter, 0, lumIter)*rgbWeight[lumIter];
       }
 
       for(int lumIter = 0; lumIter < rgbNum; lumIter++){
-	outGrey(widthIter, heightIter, 0, lumIter) = (unsigned char)lum;
+	outGrey(wIter, hIter, 0, lumIter) = (unsigned char)lum;
       }
     }
   }
 
   return;
+}
+
+
+float getMedianGreyScale(cimg_library::CImg<unsigned char>* inGrey_p)
+{
+  const unsigned int imgWidth = inGrey_p->width();
+  const unsigned int imgHeight = inGrey_p->height();
+
+  std::vector<float>* lumVect_p = new std::vector<float>;
+
+  for(int wIter = 0; wIter < imgWidth; wIter++){
+    for(int hIter = 0; hIter < imgHeight; hIter++){
+      lumVect_p->push_back((float)inGrey_p->atXYZ(wIter, hIter, 0, 0));
+    }
+  }
+
+  std::sort(lumVect_p->begin(), lumVect_p->end());
+
+  const int vSize = lumVect_p->size();
+  return lumVect_p->at(vSize/2);
 }
